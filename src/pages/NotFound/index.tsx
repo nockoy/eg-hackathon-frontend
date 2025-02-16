d01c3fa1ed529c87db4f54a1bb347e4d501273d0
Shun Kondo <clarinetbroken0819@gmail.com>
2025-02-17 00:55:09 +0900

    notfound追加

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const NotFound = () => {
+  return <Text>NotFound</Text>;
+};
+
+const Text = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+`;
