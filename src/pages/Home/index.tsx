1d50fda13c0147e2a9cd1836a451613cc18912ea
Shun Kondo <clarinetbroken0819@gmail.com>
2025-02-17 00:09:00 +0900

    不要なファイルの削除

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Home = () => {
+  return <Text>Home</Text>;
+};
+
+const Text = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+`;
