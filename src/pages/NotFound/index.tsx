d6b147f2b68f3b6276fa5a4da33039fdb31b7a01
nockoy <clarinetbroken0819@gmail.com>
2025-02-23 01:34:32 +0900

    notfound更新

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,19 +1,45 @@
 import { FC } from "react";
 import styled from "styled-components";
-import { DefaultLayout } from "../../components/Layout/DefaultLayout";
 import { Stack } from "@mantine/core";
+import { useNavigate } from "react-router-dom";
 
 export const NotFound: FC = () => {
+  const navigate = useNavigate();
+
   return (
-    <DefaultLayout>
-      <Stack align="center" justify="center" gap="8">
-        <_Text>NotFound</_Text>
+    <_Wrapper>
+      <Stack align="center" justify="center" gap="24">
+        <_Text>
+          Opps! <br />
+          お探しのページは見つかりませんでした
+        </_Text>
+        <_Button onClick={() => navigate("/")}>
+          <_Text>ホームに戻る</_Text>
+        </_Button>
       </Stack>
-    </DefaultLayout>
+    </_Wrapper>
   );
 };
+
+const _Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const _Button = styled.div`
+  background-color: #3400c2;
+  color: #fff;
+  padding: 10px 20px;
+  border-radius: 16px;
+  cursor: pointer;
+`;
 
 const _Text = styled.div`
   font-size: 20px;
   font-weight: bold;
+  text-align: center;
 `;
