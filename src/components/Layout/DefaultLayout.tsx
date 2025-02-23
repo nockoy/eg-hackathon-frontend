35564a7bc807560792c394b83107215a4c05024d
nockoy <clarinetbroken0819@gmail.com>
2025-02-23 16:27:15 +0900

    微修正

--- src/components/Layout/DefaultLayout.tsx
+++ src/components/Layout/DefaultLayout.tsx
@@ -13,7 +13,7 @@ export const DefaultLayout: FC<{ children: ReactNode }> = ({
 
 const _Wrapper = styled.div`
   width: 100vw;
-  height: 100vh;
+  height: 100svh;
   display: flex;
   flex-direction: column;
   align-items: center;
