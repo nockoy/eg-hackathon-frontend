819b2025465d6b960a1460c058c2f0ba5ab6aad2
nockoy <clarinetbroken0819@gmail.com>
2025-02-25 19:16:22 +0900

    progress修正

--- src/features/home/components/ProgressBar.tsx
+++ src/features/home/components/ProgressBar.tsx
@@ -1,21 +1,34 @@
 import { Progress } from "@mantine/core";
+import styled from "styled-components";
 
 type ProgressBarProps = {
   progress: number;
 };
 
 export const ProgressBar = ({ progress }: ProgressBarProps) => {
-  const displayProgress = progress < 4 ? 4 : progress;
 
   return (
-    <Progress.Root w="100%" size={16} radius={8}>
-      <Progress.Section
-        value={displayProgress}
-        color="orange"
-        style={{ borderRadius: 8 }}
-      >
-        <Progress.Label></Progress.Label>
-      </Progress.Section>
-    </Progress.Root>
+    <div style={{ position: "relative" }}>
+      <Progress.Root w="100%" size={16} radius={8}>
+        <Progress.Section
+          value={progress}
+          color="orange"
+          style={{ borderRadius: 8 }}
+        >
+          <Progress.Label></Progress.Label>
+        </Progress.Section>
+      </Progress.Root>
+      <_Period />
+    </div>
   );
 };
+
+const _Period = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 16px;
+  width: 16px;
+  border-radius: 50%;
+  background-color: #fd7e14;
+`;
