577307920f903ce64ee9de3d4cab8f707c9c2015
nockoy <clarinetbroken0819@gmail.com>
2025-02-26 15:03:03 +0900

    チャレンジ詳細を取得できるようにした

--- src/features/commit/pages/index.tsx
+++ src/features/commit/pages/index.tsx
@@ -1,33 +1,86 @@
 import { Stack, Text } from "@mantine/core";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import styled from "styled-components";
 import { formatDate } from "../../../util/formatDate";
+import api from "../../../api/axios";
+import { useParams } from "react-router-dom";
 
-const fetchMockData = () => {
-  return [
-    {
-      challenge_id: 2,
-      commits: [
-        new Date("2025-02-24T00:00:00.000Z"),
-        new Date("2025-02-25T00:00:00.000Z"),
-      ],
-      created_at: new Date("2025-02-24T00:00:00.000Z"),
-      deposit: 2000,
-      description:
-        "2010年度〜2019年度の過去問を解く！\n得点率8割越えを目指したい。",
-      end_date: new Date("2025-03-03T12:35:00.000Z"),
-      refund: 0,
-      status: "ongoing",
-      title: "本番までに過去問10年分解く",
-      max_commit: 5,
-    },
-  ];
+// const fetchMockData = () => {
+//   return [
+//     {
+//       challenge_id: 2,
+//       commits: [
+//         new Date("2025-02-24T00:00:00.000Z"),
+//         new Date("2025-02-25T00:00:00.000Z"),
+//       ],
+//       created_at: new Date("2025-02-24T00:00:00.000Z"),
+//       deposit: 2000,
+//       description:
+//         "2010年度〜2019年度の過去問を解く！\n得点率8割越えを目指したい。",
+//       end_date: new Date("2025-03-03T12:35:00.000Z"),
+//       refund: 0,
+//       status: "ongoing",
+//       title: "本番までに過去問10年分解く",
+//       max_commit: 5,
+//     },
+//   ];
+// };
+
+type ChallengeData = {
+  challenge_id: number;
+  user_id: number;
+  group_id: number;
+  title: string;
+  image_url: string;
+  description: string;
+  max_commit: number;
+  commit: number;
+  commit_rate: number;
+  status: string;
+  end_date: string;
+  public: boolean;
+  official: boolean;
+  updated_at: string;
+  created_at: string;
+  commits: Date[];
+  deposit: number;
+};
+
+const fetchData = async (challengeId: number) => {
+  const res = await api.get(`/challenges?challenge_id=${challengeId}`);
+  return res.data;
 };
 
 export const Index: FC = () => {
-  const mockData = fetchMockData();
+  const { id } = useParams();
+  console.log("id", id);
+  const challengeId = Number(id);
+  const [data, setData] = useState<ChallengeData | null>(null);
 
-  const data = mockData[0];
+  const fetchDataAndLog = async () => {
+    try {
+      const fetchedData = await fetchData(challengeId);
+      setData(fetchedData);
+      console.log(fetchedData);
+    } catch (error) {
+      console.error("データの取得中にエラーが発生しました:", error);
+      setData(null);
+    }
+  };
+  console.log("data", data);
+
+  useEffect(() => {
+    fetchDataAndLog();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [challengeId]);
+
+  if (!data) {
+    return (
+      <_Stack>
+        <Text>データがありません</Text>
+      </_Stack>
+    );
+  }
 
   return (
     <_Stack>
@@ -38,16 +91,16 @@ export const Index: FC = () => {
               {data.title}
             </Text>
             <Text size="md" fw={400}>
-              開始：{formatDate(data.created_at.toString())}
+              開始：{formatDate(data.created_at?.toString() || "")}
             </Text>
             <Text size="md" fw={400}>
-              期限：{formatDate(data.end_date.toString())}
+              期限：{formatDate(data.end_date?.toString() || "")}
             </Text>
             <Text size="md" fw={400}>
               回数：{data.max_commit}回
             </Text>
             <Text size="md" fw={400}>
-              預かり金額：{data.deposit.toLocaleString()}円
+              預かり金額：{data.deposit?.toLocaleString()}円
             </Text>
             <Text size="md" fw={400}>
               やること：{data.description}
